823ce53698acab36571fd83f05b9afbf31f610ee
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2024-03-05 14:43:38 +0200

    Task1 add SequenceGenerator

---
+++ src/main/java/de/telran/multithread/atomic/SequenceGenerator.java
@@ -0,0 +1,27 @@
+package de.telran.multithread.atomic;
+
+import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SequenceGenerator
+{
+    private BigInteger           MULTIPLIER;
+    private AtomicReference<BigInteger> element;
+
+    public SequenceGenerator()
+    {
+//		if (MULTIPLIER == null)
+        MULTIPLIER = BigInteger.valueOf(2);
+        element = new AtomicReference<BigInteger>(BigInteger.ONE);
+    }
+    public BigInteger next()
+    {
+        BigInteger value;
+        BigInteger next;
+        do {
+            value = element.get();
+            next = value.multiply(MULTIPLIER);
+        } while (!element.compareAndSet(value, next));
+        return value;
+    }
+}
